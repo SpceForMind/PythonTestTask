2d77f78313db7d7febe9d500ea80936caa1eb9bf
SpceForMind <casha_davydov@mail.ru>
2018-04-18 21:17:56 +0300

    Add programm for clean test files system

---
+++ TestTaskOnPython/clean_file_system.py
@@ -0,0 +1,36 @@
+
+import os
+
+
+def DeleteTxtFiles(matrix_files, matrix_dir_ways):
+    root_dir = os.getcwd()
+    
+    for i in range(len(matrix_dir_ways)): 
+        for file_name in matrix_files[i]:
+            cur_file = matrix_dir_ways[i] + '/' + file_name
+            if str(cur_file).endswith('.txt'):
+                print("rm", cur_file)
+                os.remove(cur_file)
+
+def DeleteTestDirs(matrix_dir_ways):
+    root_dir = os.getcwd()
+    
+    #start with last directory of level deep
+    for i in range(len(matrix_dir_ways) - 1, -1, -1):
+        if matrix_dir_ways[i]!= root_dir:
+            print("rm -rf", matrix_dir_ways[i])
+            os.rmdir(matrix_dir_ways[i])
+
+
+
+if __name__ == "__main__":
+    root_dir = os.getcwd()
+    matrix_files = []
+    matrix_dir_ways = []
+    
+    for dir_w, dirs, files in os.walk(root_dir):
+        matrix_dir_ways.append(dir_w)
+        matrix_files.append(files)
+
+    DeleteTxtFiles(matrix_files, matrix_dir_ways)
+    DeleteTestDirs(matrix_dir_ways)
