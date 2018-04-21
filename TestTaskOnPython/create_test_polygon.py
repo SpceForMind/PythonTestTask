3e3c78915cc1fae60c9a91bc6721cff3480e55e2
SpceForMind <casha_davydov@mail.ru>
2018-04-22 00:41:37 +0300

    Debug create polygon

--- TestTaskOnPython/create_test_polygon.py
+++ TestTaskOnPython/create_test_polygon.py
@@ -5,12 +5,15 @@ import argparse
 def CreateTestPolygon(matrix_ways):
     root = os.getcwd() + '/'
     new_dir = root
-
+    
     for way in matrix_ways:
-        new_dir += os.path.split(str(way))[0]
-        file_name = os.path.split(str(way))[1]
-        if not os.path.isdir(new_dir):
-            os.mkdir(new_dir)
+        matrix_embded_way = []
+        matrix_embded_way += os.path.split(way)[0].split('/')
+        for embded_way in matrix_embded_way:
+            new_dir += embded_way + '/'
+            if not os.path.isdir(new_dir):
+                os.mkdir(new_dir)
+        file_name = os.path.split(way)[1]
         if file_name.endswith('.txt'):
             new_file =  open(new_dir + '/' + file_name, 'w')
             new_file.close()
@@ -20,10 +23,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('ways', nargs = '+')
     namespace = parser.parse_args()
-
+    
     matrix_ways = []
 
     for way in namespace.ways:
        matrix_ways.append(str(way))
-    
+    print(matrix_ways)
     CreateTestPolygon(matrix_ways)
