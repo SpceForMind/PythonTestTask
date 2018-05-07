eadfff3e25efb850e7afce3b2b2f36b7dded47dd
SpceForMind <casha_davydov@mail.ru>
2018-05-07 23:23:27 +0300

    Change clean_test_file_system

--- TestTaskOnPython/clean_test_file_system.py
+++ TestTaskOnPython/clean_test_file_system.py
@@ -3,39 +3,39 @@ import os
 
 
 matrix_files = []  
-matrix_dir_ways = []
+directory_ways = []
 
 
-def DeleteTxtFiles(matrix_files, matrix_dir_ways):
+def DeleteTxtFiles(matrix_files, directory_ways):
     root_dir = os.getcwd()
     
-    for i in range(len(matrix_dir_ways)): 
+    for i in range(len(directory_ways)): 
         for file_name in matrix_files[i]:
-            cur_file = matrix_dir_ways[i] + '/' + file_name
+            cur_file = directory_ways[i] + '/' + file_name
             #delete files only from root directory
             if str(cur_file).endswith('.txt') and os.path.split(cur_file)[0] != root_dir:
                 print("rm", cur_file)
                 os.remove(cur_file)
 
 
-def DeleteTestDirs(matrix_dir_ways):
+def DeleteTestDirs(directory_ways):
     root_dir = os.getcwd()
     
     #start with last directory of level deep
-    for i in range(len(matrix_dir_ways) - 1, -1, -1):
-        if matrix_dir_ways[i]!= root_dir:
-            print("rm -rf", matrix_dir_ways[i])
-            os.rmdir(matrix_dir_ways[i])
+    for i in range(len(directory_ways) - 1, -1, -1):
+        if directory_ways[i]!= root_dir:
+            print("rm -rf", directory_ways[i])
+            os.rmdir(directory_ways[i])
 
-def WalkForRootDirectory(matrix_dir_ways, matrix_files):
+def WalkForRootDirectory(directory_ways, matrix_files):
     root_dir = os.getcwd()
     for dir_w, dirs, files in os.walk(root_dir):
-        matrix_dir_ways.append(dir_w)
+        directory_ways.append(dir_w)
         matrix_files.append(files)
 
 
 
 if __name__ == "__main__":
-    WalkForRootDirectory(matrix_dir_ways, matrix_files)
-    DeleteTxtFiles(matrix_files, matrix_dir_ways)
-    DeleteTestDirs(matrix_dir_ways)
+    WalkForRootDirectory(directory_ways, matrix_files)
+    DeleteTxtFiles(matrix_files, directory_ways)
+    DeleteTestDirs(directory_ways)
